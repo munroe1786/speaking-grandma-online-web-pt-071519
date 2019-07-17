9fb24df2781c95836757a885810c1e8275163816
Eliza Munroe <era3munroe@gmail.com>
2019-07-17 18:45:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  
-  
-  if phrase.downcase == "phrase"
+  if phrase == "I LOVE YOU GRANDMA!"
+     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.downcase == "phrase"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase == "phrase"
     return "NO, NOT SINCE 1938!"
